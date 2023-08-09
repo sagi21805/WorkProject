ae13aae1f6eb263494d31094da1cb545383fdcb3
sagi <sagi21805@gmail.com>
2023-08-10 01:30:44 +0300

    10.8

--- gpu.py
+++ gpu.py
@@ -1,40 +1,42 @@
 import time
-from numba import jit
+from numba import njit
+from numba import prange
 import numpy as np
 import numpy.lib.stride_tricks
 import cv2
-import multiprocessing.pool
 from cProfile import Profile
 from pstats import SortKey, Stats
+import multiprocessing.pool
 
-
-@jit(nopython=True)
-def func(img: np.ndarray, s):
-    thresh = 255*s*s*0.3
+# important!! - for *very* large operations (like s > 15 or even 20) parallel = True improves the calcs by a lot
+@njit(fastmath=True)
+def func(img: np.ndarray, s, thresh):
     blocked = numpy.lib.stride_tricks.sliding_window_view(img, (s, s))
-    new = np.zeros((blocked.shape[0], blocked.shape[1]))
-    for i, row in enumerate(blocked):
-        for j, block in enumerate(row):
-            if np.sum(block) > thresh:
+    new = np.zeros((blocked.shape[0], blocked.shape[1]), np.uint8)
+    for i in prange(len(blocked)):
+        for j in prange(len(blocked[0])):
+            if np.sum(blocked[i][j]) > thresh:
                 new[i][j] = 255
     return new
 
 def last():    
     v = cv2.VideoCapture(0)
+    s = 2
+    thresh = 255*s*s*0.3
     while True:
         suc, img = v.read()  
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img = cv2.resize(img, (1280, 720))
         st = time.time()
-        new = func(img, 3)
+        new = func(img, s, thresh)
+        print(time.time() - st)
         cv2.imshow("!", new)
         cv2.waitKey(1)
 
-with Profile() as profile:
-    print(f"{last() = }")
-    (
-        Stats(profile).strip_dirs().sort_stats(SortKey.CALLS).print_stats()
-    )
+last()
+        
+
+
     
 
 
