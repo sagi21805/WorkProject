78852b698ca3b9fd0b5305a2d9946cdc14ec87de
sagi or <sagi21805@gmail.com>
2023-08-27 16:05:40 +0300

    commit

--- main.py
+++ main.py
@@ -7,26 +7,25 @@ import numpy as np
 if __name__ == "__main__":
     multiprocessing.freeze_support()
     video = cv2.VideoCapture(0)
-    # while True:
-        # success, liveImg = video.read()
+    while True:
+        success, liveImg = video.read()
     # recognize circles
 
-    liveImg = cv2.imread("./Images/0003.jpg")
 
     # if success:
 
 
-    print("started")
-    # circleImg = RectImg(cv2.resize(liveImg, (1536, 864)))
-    circleImg = RectImg(liveImg)
-    circleImg.main(2, np.sum)
-    circleImg.mark()
+        print("started")
+        # circleImg = RectImg(cv2.resize(liveImg, (1536, 864)))
+        circleImg = RectImg(liveImg)
+        circleImg.main(2, np.sum)
+        circleImg.mark()
 
-        # success, liveImg = video.read()
-    cv2.imshow("marked", circleImg.markedImg)
-    cv2.imshow("p", circleImg.prepedImg)
-    cv2.waitKey(0)
-    # cv2.imshow("r", circleImg.markedImg)
+            # success, liveImg = video.read()
+        cv2.imshow("marked", circleImg.markedImg)
+        cv2.imshow("p", circleImg.prepedImg)
+        cv2.waitKey(1)
+        # cv2.imshow("r", circleImg.markedImg)
     # cv2.waitKey(0)
             
 
