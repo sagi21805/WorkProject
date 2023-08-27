2209fac82a6e022cafaa03c4ceb2eacd31df137d
sagi or <sagi21805@gmail.com>
2023-08-27 15:24:57 +0300

    27.8

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
 import cv2 
-from algs.algCircle import CircleImg
-import multiprocessing
+from algs.algRectangle import RectImg
 import numpy as np
+# video = cv2.VideoCapture(0)
 
-if __name__ == "__main__":
-    video = cv2.VideoCapture(0)
-    while True:
-        success, liveImg = video.read()
-        # recognize circles
+# recognize circles
 
-        # liveImg = cv2.imread("00010.jpg")
+liveImg = cv2.imread("0004.jpg")
 
-            # if success:
+# if success:
 
 
-        print("started")
-        # circleImg = RectImg(cv2.resize(liveImg, (1536, 864)))
-        circleImg = CircleImg(liveImg)
-        circleImg.main(1)
-        circleImg.mark()
-        # cv2.namedWindow("marked")  
-        # cv2.setMouseCallback("marked",circleImg.markByClicking)  
+print("started")
+# circleImg = RectImg(cv2.resize(liveImg, (1536, 864)))
+circleImg = RectImg(liveImg)
+circleImg.mainRect(6, np.sum)
+cv2.namedWindow("marked")  
+cv2.setMouseCallback("marked",circleImg.markByClicking)  
 
-                # success, liveImg = video.read()
-        cv2.imshow("marked", circleImg.markedImg)
-        cv2.waitKey(1)
-            # cv2.imshow("r", circleImg.markedImg)
-            # cv2.waitKey(0)
+
+while True:
+    # success, liveImg = video.read()
+    cv2.imshow("marked", circleImg.markedImg)
+    cv2.imshow("p", circleImg.prepedImg)
+    cv2.waitKey(1)
+# cv2.imshow("r", circleImg.markedImg)
+# cv2.waitKey(0)
             
 
 
